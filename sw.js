779e237c8338a1bbff0535272714d1f92973b452
steirergeorg-create <steirergeorg@gmail.com>
2026-05-22 09:16:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-const CACHE = 'bf-manager-v1';
+const CACHE = 'bf-manager-v2';
 const ASSETS = ['/', '/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
 });
 
 self.addEventListener('fetch', e => {
